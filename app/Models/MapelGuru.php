f0c23918085f0da45970fa03945f9773d1fa4320
bobbgede27 <bobbygede27@gmail.com>
2023-07-22 16:41:08 +0800

    revisi-2

---
+++ app/Models/MapelGuru.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class MapelGuru extends Model
+{
+    use HasFactory;
+    protected $guarded = [], $table = 'tb_mapel_guru';
+
+    
+}
--- resources/views/guru/dashboard.blade.php
+++ resources/views/guru/dashboard.blade.php
@@ -9,60 +9,34 @@
         @endif
       <div class="card p-2">
         <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-          <h5 class="m-0 font-weight-bold text-primary">Informasi</h5>
+          <h5 class="m-0 font-weight-bold text-primary">Informasi Beranda</h5>
         </div>
         <div class="row">
             <div class="col-xl-3 col-md-6 mb-4">
-            <a href="/listsiswa/1" class="text-decoration-none">
-                <div class="card h-100">
-                    <div class="card-body">
-                    <div class="row no-gutters align-items-center">
-                        <div class="col mr-2">
-                        <div class="text-xs font-weight-bold text-uppercase mb-1">Siswa Kelas 6 A</div>
-                        <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">{{$siswa6A}}</div>
+                <a href="/dashboard-detail" class="text-decoration-none">
+                    <div class="card h-100">
+                        <div class="card-body">
+                        <div class="row no-gutters align-items-center">
+                            <div class="col mr-2">
+                            <div class="text-xs font-weight-bold text-uppercase mb-1">Kelas 6</div>
+                            </div>
+                            <div class="col-auto">
+                            <i class="fas fa-users fa-2x text-info"></i>
+                            </div>
                         </div>
-                        <div class="col-auto">
-                        <i class="fas fa-users fa-2x text-info"></i>
                         </div>
                     </div>
-                    </div>
-                </div>
-            </a>
+                </a>
             </div>
-            <div class="col-xl-3 col-md-6 mb-4">
-            <a href="/listsiswa/2" class="text-decoration-none">
-                <div class="card h-100">
-                    <div class="card-body">
-                    <div class="row no-gutters align-items-center">
-                        <div class="col mr-2">
-                        <div class="text-xs font-weight-bold text-uppercase mb-1">Siswa Kelas 6 B</div>
-                        <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">{{$siswa6B}}</div>
-                        </div>
-                        <div class="col-auto">
-                        <i class="fas fa-users fa-2x text-info"></i>
-                        </div>
-                    </div>
-                    </div>
-                </div>
-            </a>
-            </div>
-            <div class="col-xl-3 col-md-6 mb-4">
-            <a href="/listsiswa/3" class="text-decoration-none">
-                <div class="card h-100">
-                    <div class="card-body">
-                    <div class="row no-gutters align-items-center">
-                        <div class="col mr-2">
-                        <div class="text-xs font-weight-bold text-uppercase mb-1">Siswa Kelas 6 C</div>
-                        <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">{{$siswa6C}}</div>
-                        </div>
-                        <div class="col-auto">
-                        <i class="fas fa-users fa-2x text-info"></i>
-                        </div>
-                    </div>
-                    </div>
-                </div>
-            </a>
-            </div>
+        </div>
+        <div class="card-footer"></div>
+      </div>
+    </div>
+  </div>
+
+@endsection
+
+{{-- <div class="row">
             <div class="col-xl-3 col-md-6 mb-4">
             <a href="/tugas" class="text-decoration-none">
                 <div class="card h-100">
@@ -114,10 +88,4 @@
                 </div>
             </a>
             </div>
-        </div>
-        <div class="card-footer"></div>
-      </div>
-    </div>
-  </div>
-
-@endsection
+        </div> --}}
