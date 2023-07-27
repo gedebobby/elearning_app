2619cb62d9c273cd5cfe6659ddc6d3abf7e15826
bobbgede27 <bobbygede27@gmail.com>
2023-07-27 20:48:31 +0800

    sdhsjfff

--- resources/views/siswa/materiSiswa.blade.php
+++ resources/views/siswa/materiSiswa.blade.php
@@ -17,12 +17,12 @@
                       <div class="card h-100">
                           <div class="card-body">
                           <div class="row no-gutters align-items-center">
-                              <div class="col mr-2">
-                              <div class="text-xs font-weight-bold text-uppercase mb-1">{{$std->mapel}}</div>
-                              {{-- <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">{{$countGuru}}</div> --}}
+                            <div class="col-3">
+                              <i class="fas fa-book fa-2x text-info"></i>
                               </div>
                               <div class="col-auto">
-                              <i class="fas fa-book fa-2x text-info"></i>
+                              <div class="text-xs font-weight-bold text-uppercase mb-1">{{$std->mapel}}</div>
+                              {{-- <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">{{$countGuru}}</div> --}}
                               </div>
                           </div>
                           </div>
